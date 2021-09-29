18314df728b0432eddea745f34affcb501120826
murzinairina <90762063+murzinairina@users.noreply.github.com>
2021-09-29 11:16:40 +0300

    Add files via upload

---
+++ лаб4_4.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    setlocale(0, "");
+    int a, b;
+    std::cout << "Введите a и b: " << "\n";
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << "Сумма квадратов чисел равна: " << "\n";
+    std::cout << pow(a,2) + pow(b,2) << "\n";
+    std::cout << "Разность квадратов чисел равна: " << "\n";
+    std::cout << pow(a, 2) - pow(b, 2) << "\n";
+    std::cout << "Произведение квадратов чисел равно: " << "\n";
+    std::cout << pow(a, 2) * pow(b, 2) << "\n";
+    std::cout << "Частное квадратов чисел равно: " << "\n";
+    std::cout << pow(a, 2) / pow(b, 2) << "\n";
+}
+
