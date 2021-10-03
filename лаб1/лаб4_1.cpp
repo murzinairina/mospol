ed4d400794805878cab42d40ccc6aaeb0f8098cd
murzinairina <90762063+murzinairina@users.noreply.github.com>
2021-10-03 17:11:24 +0300

    Add files via upload

---
+++ лаб1/лаб4_1.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    setlocale(0, "");
+    int a, b;
+    std::cout << "Введите стороны прямоугольника:" <<"\n";
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << "Площадь прямоугольника равна: ";
+    std::cout << a * b << "\n";
+    std::cout << "Параметр прямоугольника равен: ";
+    std::cout << (a + b) * 2 << "\n";
+
+    
+}
+
